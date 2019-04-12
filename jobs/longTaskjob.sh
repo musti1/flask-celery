21c33d5c64cd829b2da03f841ce811dbff81a653
Admin Carbonteq <admin@carbonteq.com>
2019-04-12 18:33:28 +0500

    Workers Logging + Popen Fix

---
+++ jobs/longTaskjob.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+    sleep 1
+    echo "Name param given is " $1
+done
--- workerB.py
+++ workerB.py
@@ -1,6 +1,11 @@
 from celery import Celery
 from workerBTask import WorkerBTasks
+import celstash
+import logging
 
+celstash.configure(logstash_host='logstash', logstash_port=9999)
+logger = celstash.new_logger('flask-celery')
+logger.setLevel(logging.INFO)
 # Celery configuration
 CELERY_BROKER_URL = 'amqp://rabbitmq:rabbitmq@rabbit:5672/'
 CELERY_RESULT_BACKEND = 'rpc://'
@@ -10,9 +15,9 @@ celery = Celery('workerB', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKE
 
 @celery.task()
 def counter_task():
-    return WorkerBTasks.counter_task()
+    return WorkerBTasks.counter_task(logger)
 
 
 @celery.task()
 def addition_task():
-    return WorkerBTasks.sum_task()
+    return WorkerBTasks.sum_task(logger)
