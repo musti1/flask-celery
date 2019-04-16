c5d2358c0f60088ac9faca82b8465128965d2483
Admin Carbonteq <admin@carbonteq.com>
2019-04-16 17:19:31 +0500

    Broker Custom Messages

--- jobs/workerBJobs.py
+++ jobs/workerBJobs.py
@@ -11,17 +11,19 @@ class WorkerBJobs:
     def __init__(self, logger):
         self.logger = logger
 
-
     def exit_gracefully(self, signum, frame):
         self.logger.info(f'Breaking out of the loop')
         self.logger.info(f'Current state before exit {self.current_counter}')
         self.logger.info(f'Gracefully exiting')
         self.kill_now = True
 
-    def counter_task(self):
-        self.logger.info(f'Executing counter task')
+    def counter_task(self, publisher, queue):
+        publisher({'Message': 'Executing Counter Task'}, routing_key=queue.routing_key,
+                  declare=[queue])
         while True:
-            value = 'The current value is {}\n'.format(self.current_counter)
+            value = '{}'.format(self.current_counter)
+            publisher({'Message': {'Current_value': value, 'task_id': queue.routing_key}}, routing_key=queue.routing_key,
+                      declare=[queue])
             self.logger.info(value)
             sleep(1)
             self.current_counter += 1
--- workerB.py
+++ workerB.py
@@ -2,6 +2,7 @@ import signal, os
 from celery import Celery
 from jobs.workerBJobs import WorkerBJobs
 import celstash
+from kombu import Connection, Producer, Queue
 import logging
 
 celstash.configure(logstash_host='logstash', logstash_port=9999)
@@ -19,11 +20,21 @@ workerBjobs = WorkerBJobs(logger)
 @celery.task()
 def counter_task():
     signal.signal(signal.SIGTERM, workerBjobs.exit_gracefully)
-    result = workerBjobs.counter_task()
+    logs_queue = Queue(counter_task.request.id, routing_key=counter_task.request.id)
+    kombuConnection = Connection('amqp://rabbitmq:rabbitmq@rabbit:5672//')
+    producer = Producer(kombuConnection, auto_declare=True)
+    publish = kombuConnection.ensure(producer, producer.publish, errback=errback, max_retries=3)
+    result = workerBjobs.counter_task(publish, logs_queue)
     if not result:
+        publish({'Exception': 'Exiting Gracefully'}, routing_key=logs_queue.routing_key, declare=[logs_queue])
         raise Exception("Exiting from here ")
 
 
 @celery.task()
 def addition_task():
     return workerBjobs.sum_task()
+
+
+def errback(exc, interval):
+    logger.error('Error: %r', exc, exc_info=1)
+    logger.error('retiers in second', interval)
